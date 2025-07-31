9f31e3ccd819e2ac02cce04873c4a3b22570125d
Cammy <agoni437@163.com>
2025-07-31 16:32:52 +0800

    项目初始化

---
+++ src/main/java/com/aetherbuilder/nocode/AetherbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.aetherbuilder.nocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherbuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AetherbuilderApplication.class, args);
+    }
+
+}
