93d9c5bcb4c6b91524a500e927b9cf41dff3fa8c
Cammy <agoni437@163.com>
2025-08-02 21:10:32 +0800

    AI生成应用的功能实现SSE改造

--- src/main/java/com/aetherbuilder/nocode/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/aetherbuilder/nocode/ai/AiCodeGeneratorServiceFactory.java
@@ -1,6 +1,7 @@
 package com.aetherbuilder.nocode.ai;
 
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,20 @@ public class AiCodeGeneratorServiceFactory {
     @Resource
     private ChatModel chatModel;
 
+    @Resource
+    private StreamingChatModel streamingChatModel;
+
+    // 定义一个Spring Bean，用于提供AiCodeGeneratorService实例
     @Bean
     public AiCodeGeneratorService aiCodeGeneratorService() {
-        return AiServices.create(AiCodeGeneratorService.class, chatModel);
+        // 使用AiServices构建器创建AiCodeGeneratorService对象
+        return AiServices.builder(AiCodeGeneratorService.class)
+                // 设置聊天模型，提供给AI代码生成功能使用
+                .chatModel(chatModel)
+                // 设置流式聊天模型，用于支持实时返回生成内容
+                .streamingChatModel(streamingChatModel)
+                // 构建并返回AiCodeGeneratorService实例
+                .build();
     }
+
 }
